29089cc430885822075e145cebcc067198927abd
arifsiddiqui295 <arifsiddiqui2905@gmail.com>
2024-04-08 00:32:21 +0530

    solved title

--- src/components/Typeanimation.jsx
+++ src/components/Typeanimation.jsx
@@ -7,13 +7,13 @@ const Typeanimation = () => {
         // Same substring at the start will only be typed out once, initially
         'Welcome to Typing Verse',
         1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Hit Enter to Start',
+        'Click Start To check Speed',
         1000,
         'Top Users on LeaderBoard',
         1000,
       ]}
       speed={50}
-      className="text-4xl text-black font-bold uppercase "
+      className="text-2xl sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl text-black font-bold uppercase"
       repeat={Infinity}
     />
   );
